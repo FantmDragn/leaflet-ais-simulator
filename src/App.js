cf087e3255db4db738b7c5fcfdd354239bc2e546
FantmDragn <fantmdragn@gmail.com>
2025-02-27 15:17:57 -0700

    changed ship icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
+import { MapContainer, TileLayer, CircleMarker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const mapStyle = { height: "100vh", width: "100%" };
